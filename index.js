b6318f12edef9df7545e87b24f2b01818e5279fb
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-07 18:25:42 +0530

    Install Ejs and set up views in root/index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 
 const app = express()
 const port = 8000;
-
+//this will set ejs as the view engine
+app.set('view engine', 'ejs');
+// this will set views to look for views in the views folder
+app.set('views', './views');
 
 //this tells the index/root that all routes will be handled by index.js files in routes folder
 // ./routes and ./routes/index.js are the same thing
