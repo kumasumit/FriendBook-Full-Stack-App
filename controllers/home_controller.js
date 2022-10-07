e7d9693308705b9ff25af4e5cc58dc6d1b4987be
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-07 18:12:05 +0530

    Set up controllers and routes for / home route

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+//Action 1 for home '/'
+module.exports.home = function(req, res){
+    return res.send('<h1>FriendBook is up and running</h1>');
+}
--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.get('/', (req, res) => {
 
 //this tells the index/root that all routes will be handled by index.js files in routes folder
 // ./routes and ./routes/index.js are the same thing
+//this tells the app to fetch all the routes from index file in routes folder
+//this acts as a middleware for routes
 app.use('/', require('./routes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
