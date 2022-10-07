b96484d1513a07b9fec12fafea7438e6912ca3f5
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-07 19:51:26 +0530

    Render Sign-In and Sign-Up pages

--- controllers/users_conroller.js
+++ controllers/users_conroller.js
@@ -1,6 +1,23 @@
+const User = require('../models/users');
+
+
 //Action 1 for /users/profile
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: "User Profile"
     })
+}
+
+//Action 2 to render sign-up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+        title:"FriendBook||Sign Up"
+    });
+}
+
+//Action 3 to render sign-in page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "FriendBook||Sign In"
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router()
 //include the users_controller
 const usersController = require('../controllers/users_conroller');
 router.get('/profile', usersController.profile);
+//router to render sign-in and sign-up pages
+router.get('/sign-in', usersController.signIn);
+router.get('sign-up', usersController.signUp);
 
 
 module.exports = router
