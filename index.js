b72bd4ffceca54743a514d6551359ae6aa215cf1
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-11 21:16:23 +0530

    Install and set up connect-mongo

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo');
 const app = express()
 const port = 8000;
 
@@ -45,12 +46,18 @@ app.use(session({
   cookie: {
       maxAge: (1000*60*100)
       //this is the maxage in milliseconds
-  }
+  },
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/FriendBook-Development' })
 
 }))
 //here we initialize passport and passport session as soon as the app loads
 app.use(passport.initialize());
 app.use(passport.session());
+//here as soon as the passport is initialized and the session is created for logged in user,
+//we save the user in locals variable to display it in views
+app.use(passport.setAuthenticatedUser);
+//this will set the logged-in user in locals.user for the views as soon as the app loads.
+
 //this tells the index/root that all routes will be handled by index.js files in routes folder
 // ./routes and ./routes/index.js are the same thing
 //this tells the app to fetch all the routes from index file in routes folder
