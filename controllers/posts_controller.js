332e03c7d42f8016f497c96508fa279a89a29fcb
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-13 12:27:23 +0530

    Delete post and associated comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post = require('../models/posts');
-
+const Comment = require('../models/comments');
 //Action 1 to create a Post by a signed-in user
 module.exports.create = function (req, res) {
     Post.create({
@@ -14,4 +14,27 @@ module.exports.create = function (req, res) {
         }
         return res.redirect('back');
     })
+}
+
+//Action 2 to delete a Post by a signed-in user
+//delete a post and all associated comments
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the object id into string
+        //here we check if user requesting to delete the post is the same user who created/owns the post
+        if (post.user == req.user.id){
+            //if the ids match, both users are same, then delete the post
+            post.remove();
+            //post.remove() will delete the post
+            //go inside Comment schema and search all the comments belonging to a particular post and delete all the linked comments with the deleted post
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+            //Comment.deleteMany() will delete more than one comment
+        }
+        //if users dont match, send the control back
+        else{
+            return res.redirect('back');
+        }
+    });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,9 @@ const passport = require('passport');
 const postsController = require('../controllers/posts_controller');
 //passport.checkAuthentication will prevent the user from creating a post if the user is not signed in
 router.post('/create',passport.checkAuthentication ,postsController.create);
+//a router to delete a specfic post, only if the user is signed in
+router.get('/destroy/:id', passport.checkAuthentication,postsController.destroy);
+//here passport.checkAuthentication checks whether the user is signed-in or not
 
 
 
