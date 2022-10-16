9428a5cdc21b4d2410e3b654c086ce73f506517c
Kumar Sumit <kumarsumit1989@gmail.com>
2022-10-16 20:13:27 +0530

    Convert home_controller code to async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,32 +1,38 @@
 const Post = require("../models/posts");
 const User = require("../models/users");
+
 //Action 1 for home '/'
-module.exports.home = function (req, res) {
-  // loads posts and comments both
-  Post.find({})
-    //find all the posts from the database
-    .populate("user", "name email")
-    //populate user for each post with only name and email, not password
-    .populate({
-      path: "comments",
-      //populate comments array for each post
-      populate: {
-        //populate user inside comment for each comment inside comments array
-        path: "user",
-      },
-    })
-    .exec(function (err, posts) {
-      if (err) {
-        return handleError(err);
-      }
-      User.find({}, function (err, users) {
-        return res.render("home", {
-          title: "Home",
-          posts: posts,
-          //here posts is the populated posts with comments and user populated
-          all_users: users,
-          //here we find all the users and assign it to all_user variable in home action of home_controllers
-        });
-      });
-    });
-};
+module.exports.home = async function (req, res) {
+  try {
+    // Step 1: get posts
+    //any success response of Post.find will be stored in posts
+    let posts = await 
+          // loads posts and comments both
+          Post.find({})
+          //find all the posts from the database
+          .populate("user", "name email")
+          //populate user for each post with only name and email, not password
+          .populate({
+          path: "comments",
+        //populate comments array for each post
+        populate: {
+          //populate user inside comment for each comment inside comments array
+          path: "user",
+        },
+      })
+
+  // Step 2: get users
+  //any success response of User.find will be stored in users 
+  let users = await User.find({});
+  //User.find({}) will display all the users in the database
+  // Step 3: return posts and users to the home view page, home.ejs page
+  return res.render('home', {
+      title: " FriendBook || Home",
+      posts: posts,
+      all_users: users
+  });
+  }catch(error){
+    console.log("Error", error);
+    return;
+  }
+}  
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,7 +1,24 @@
 const Post = require('../models/posts');
 const Comment = require('../models/comments');
+
 //Action 1 to create a Post by a signed-in user
-module.exports.create = function (req, res) {
+module.exports.create = async function (req, res) {
+    try{
+        //create the post with content and userId of the user who created that post
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+            //here we dont store the complete user but only the id, since id is unique for every user, and can be used to populate the entire user at a later stage
+            //here in id we are storing the id of the user who is creating the post
+        })
+        //after creating the post we return the control back to home
+        return res.redirect('back');
+    }catch(error){
+        //if there is any error in above process, the control will go to catch block
+        // and we will log the errors in the console and return
+        console.log("Error", error);
+        return;
+    }
     Post.create({
         content: req.body.content,
         user: req.user._id
